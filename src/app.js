ada048fcb77ac93bd747f686794a61044793e592
roubertpi <roubertpimentar@gmail.com>
2020-12-08 00:20:19 -0300

    Criando APIs com NodeJs - Aula 11: Rotas

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
-'use strict'
+"use strict";
 
-const express =require('express');
-const bodyParser =require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const router = express.Router();
 
+// carregar rotas
+const indexRoutes = require ('./routes/index-route');
+const products = require ('./routes/products-route');
+
+
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configurando a Primeira rota
-const route = router.get('/',(req, res, next) => {
-    res.status(200).send({
-        title: "Node Store Api",
-        version: "0.0.1"
-    });
-});
 
 
-const create = router.post('/',(req, res, next) => {
-    res.status(201).send(req.body);
-});
-app.use ('/',route);
-app.use ('/products',create);
+
+
+
+
+app.use("/", indexRoutes);
+app.use("/products", products);
+
 module.exports = app;
